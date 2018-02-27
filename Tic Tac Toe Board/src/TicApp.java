220787605665825319711c170867859319d9c532
rogerr2 <35306165+rogerr2@users.noreply.github.com>
2018-02-26 22:59:34 -0600

    Add files via upload

---
+++ Tic Tac Toe Board/src/TicApp.java
@@ -0,0 +1,4 @@
+
+public class TicApp {
+
+}
---
+++ Tic Tac Toe Board/src/TicTacToe.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.black);
+		g.setFont(new Font(""));
+		
+		ArrayList<DrawnX> xList = new ArrayList<DrawnX>();
+		
+		for (TicTacToe.DrawnX thisX : )
+		
+		g.drawnString("X", textX.xCoord, textX.yCoord);
+		
+		DrawnX textX = new DrawnX(200,00);
+		
+		g.drawnString("0", textX.xCoord, textX.yCoord);
+	
+	class Mouse implements MouseListener {
+		
+	}
+	
+	}
+
+	class DrawnX {
+		int xCoord = 200;
+		int yCoord = 200;
+	}
+
+
+
+
+
+
+}
